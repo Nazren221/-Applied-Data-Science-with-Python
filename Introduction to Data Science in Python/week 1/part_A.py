145d094eebd2508e10b773531cef634d26ac510a
Nazren Baghirova <63057970+Nazren221@users.noreply.github.com>
2021-02-13 15:50:34 +0400

    Delete part_A.py

--- Introduction to Data Science in Python/week 1/part_A.py
+++
@@ -1,9 +0,0 @@
-import re
-def names():
-    simple_string='''Amy is 5 years old, and her sister Mary is 2 years old.
-    Ruth and Peter, their parents, have 3 kids.'''
-
-    name_list=re.findall('[A-Z][\w]*', simple_string)
-    return name_list
-
-print(names())
